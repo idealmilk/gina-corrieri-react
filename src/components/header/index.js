f86156644a2d66c455b83a8aa1779976284d2dae
Matthew Gilligan <mcgilligan97@gmail.com>
2020-12-20 11:34:39 +0000

    Style header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,12 +7,18 @@ import Logo from '../../assets/gina-logo-black.png';
 const Header = props => {
   return (
     <header className="header">
-      <div className="wrap">
-        <div className="logo">
-          <Link to="/ ">
-            <img src={Logo} alt="Gina Corrieri" />
-          </Link>
-        </div>
+      <nav id="nav-menu-left" className="nav-menu">
+        <p className="nav-link">Lookbook</p>
+        <p className="nav-link">Shop</p>
+      </nav>
+      <nav id="nav-menu-right" className="nav-menu">
+        <p className="nav-link">About</p>
+        <p className="nav-link">Account</p>
+      </nav>
+      <div className="logo">
+        <Link to="/ ">
+          <img src={Logo} alt="Gina Corrieri" />
+        </Link>
       </div>
     </header>
   );
