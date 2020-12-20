ace683e97fab2115ad9fbb003d785cee05a49597
Matthew Gilligan <mcgilligan97@gmail.com>
2020-12-20 11:49:25 +0000

    Add footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.scss';
+
+const Footer = props => {
+  return (
+    <footer className="footer">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
