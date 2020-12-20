9092a5ddd73632f920b51dad0981014445b2b4a7
Matthew Gilligan <mcgilligan97@gmail.com>
2020-12-20 10:47:40 +0000

    Add link to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.scss';
-import Logo from '../../assets/gina-logo-black.png'
+import Logo from '../../assets/gina-logo-black.png';
 
 const Header = props => {
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-          <img src={Logo} alt="Gina Corrieri" />
+          <Link to="/ ">
+            <img src={Logo} alt="Gina Corrieri" />
+          </Link>
         </div>
       </div>
     </header>
