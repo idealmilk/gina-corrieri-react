9ce0bfb1ae28e9e349eef882f4226eb82293717e
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-03 12:58:20 +0000

    Refactor email password component with redux hooks

--- src/redux/User/user.actions.js
+++ src/redux/User/user.actions.js
@@ -42,3 +42,33 @@ export const signUpUser = ({ displayName, email, password, confirmPassword }) =>
     console.log(err);
   }
 };
+
+export const resetPassword = ( { email }) => async dispatch => {
+  const config = {
+    url: 'http://localhost:3000/login'
+  };
+
+  try {
+    await auth.sendPasswordResetEmail(email, config)
+      .then(() => {
+        dispatch({
+          type: userTypes.RESET_PASSWORD_SUCCESS,
+          payload: true
+        });
+
+        console.log('Password Reset Sent');
+      })
+      .catch(() => {
+        const err = ['Email not found. Please try again.'];
+        dispatch({
+          type: userTypes.RESET_PASSWORD_ERROR,
+          payload: err
+        });
+
+        console.log('Something went wrong');
+      });
+
+  } catch(err) {
+    console.log(err);
+  }
+}
