cc785249d86fc1f82d6bbb5285c4a2e14c342589
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-06 19:29:58 +0000

    Tidy up header and admin toolbar

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,7 +4,7 @@ import { signOutUserStart } from './../../redux/User/user.actions';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 
-import Logo from './../../assets/gina-logo-black.png';
+import Logo from './../../assets/logo.png';
 
 const mapState = ({ user }) => ({
   currentUser: user.currentUser
@@ -20,32 +20,46 @@ const Header = props => {
 
   return (
     <header className="header">
-      <nav id="nav-menu-left" className="nav-menu">
-        <p className="nav-link">Lookbook</p>
-        <p className="nav-link">Shop</p>
-      </nav>
-      <nav id="nav-menu-right" className="nav-menu">
-        <p className="nav-link">About</p>
-        {currentUser && [
-          <div>
-            <span onClick={() => signOut()}>
-              <p className="nav-link">Log Out</p>
-            </span>
-            <Link to="/dashboard">
-              <p className="nav-link">Account</p>
-            </Link>
-          </div>
-        ]}
-        {!currentUser && [
-          <Link to="/login">
-            <p className="nav-link">Login</p>
+      <div className="wrap">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="SimpleTut LOGO" />
           </Link>
-        ]}
-      </nav>
-      <div className="logo">
-        <Link to="/">
-          <img src={Logo} alt="Gina Corrieri" />
-        </Link>
+        </div>
+
+        <div className="callToActions">
+
+          {currentUser && (
+            <ul>
+              <li>
+                <Link to="/dashboard">
+                  My Account
+                </Link>
+              </li>
+              <li>
+                <span onClick={() => signOut()}>
+                  LogOut
+                </span>
+              </li>
+            </ul>
+          )}
+
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">
+                  Register
+              </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  Login
+              </Link>
+              </li>
+            </ul>
+          )}
+
+        </div>
       </div>
     </header>
   );
