f73472bbc0a183fa6dd867a3cde4a2efdae2c623
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-09 16:54:57 +0000

    Add basic styling to rendered products

---
+++ src/components/ProductResults/Product/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+
+const Product = ({
+  productThumbnail,
+  productName,
+  productPrice
+}) => {
+  if (!productThumbnail || !productName ||
+          typeof productPrice === 'undefined') return null;
+
+  return (
+    <div className="product">
+      <div className="thumb">
+        <img src={productThumbnail} alt={productName} />
+      </div>
+
+      <div className="details">
+        <ul>
+          <li>
+            <span className="name">
+              {productName}
+            </span>
+          </li>
+          <li>
+            <span className="price">
+              £{productPrice}
+            </span>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  );
+};
+
+export default Product;
--- src/components/ProductResults/index.js
+++ src/components/ProductResults/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProductsStart } from './../../redux/Products/products.actions';
+import Product from './Product';
 import './styles.scss';
 
 const mapState = ({ productsData }) => ({
@@ -29,16 +30,22 @@ const ProductResult = ({ }) => {
 
   return (
     <div className="products">
+      <h1>
+        Browse Products
+      </h1>
       {products.map((product, pos) => {
         const { productThumbnail, productName, productPrice } = product;
         if (!productThumbnail || !productName ||
           typeof productPrice === 'undefined') return null;
 
+        const configProduct = {
+          productThumbnail,
+          productName,
+          productPrice
+        };
+
         return (
-          <div key={pos}>
-            {productName}
-            {productPrice}
-          </div>
+          <Product {...configProduct} />
         );
       })}
     </div>
