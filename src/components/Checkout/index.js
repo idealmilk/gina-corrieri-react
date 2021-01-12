1af08d436f0eb7c33c5371b77c493ee29e14ad76
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-12 19:49:11 +0000

    Finish cart and checkout logic and basic styling

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import {useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectCartItems } from './../../redux/Cart/cart.selectors';
+import { selectCartItems, createCartTotal } from './../../redux/Cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
 import './styles.scss';
 import Button from './../forms/Button';
+import Item from './Item';
 
 const mapState = createStructuredSelector({
-  cartItems: selectCartItems
+  cartItems: selectCartItems,
+  total: createCartTotal
 });
 
 const Checkout = ({}) => {
-  const { cartItems } = useSelector(mapState);
+  const history = useHistory();
+  const { cartItems, total } = useSelector(mapState);
 
   return (
     <div className="checkout">
@@ -19,83 +23,88 @@ const Checkout = ({}) => {
       </h1>
 
       <div className="cart">
+        {cartItems.length > 0 ? (
+          <table border="0" cellPadding="0" cellSpacing="0">
+            <tbody>
 
-        <table border="0" cellPadding="0" cellSpacing="0">
-          <tbody>
+              <tr>
+                <table className="checkoutHeader" border="0" cellPadding="0" cellSpacing="0">
+                  <tbody>
+                    <tr>
+                      <th>
+                        Product
+                      </th>
+                      <th>
+                        Description
+                      </th>
+                      <th>
+                        Quantity
+                      </th>
+                      <th>
+                        Price
+                      </th>
+                      <th>
+                        Remove
+                      </th>
+                    </tr>
+                  </tbody>
+                </table>
+              </tr>
 
-            <tr>
-              <table className="checkoutHeader" border="0" cellPadding="0" cellSpacing="0">
-                <tbody>
-                  <tr>
-                    <th>
-                      Product
-                    </th>
-                    <th>
-                      Description
-                    </th>
-                    <th>
-                      Quantity
-                    </th>
-                    <th>
-                      Price
-                    </th>
-                    <th>
-                      Remove
-                    </th>
+              <tr>
+                <table border="0" cellPadding="0" cellSpacing="0">
+                  <tbody>
+                    {cartItems.map((item, pos) => {
+                      return (
+                        <tr key={pos}>
+                          <td>
+                            <Item {...item} />
+                          </td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                </table>
+              </tr>
+
+              <tr>
+                <table algin="right" border="0" cellSpacing="0" cellPadding="10">
+                  <tr algin="right">
+                    <td>
+                      <h3>
+                        Total: £{total}
+                      </h3>
+                    </td>
                   </tr>
-                </tbody>
-              </table>
-            </tr>
-
-            <tr>
-              <table border="0" cellPadding="0" cellSpacing="0">
-                <tbody>
-                  {cartItems.map((item, pos) => {
-                    return (
-                      <tr key={pos}>
-                        <td>
-                          {item.productName}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </tr>
-
-            <tr>
-              <table algin="right" border="0" cellSpacing="0" cellPadding="10">
-                <tr algin="right">
-                  <td>
-                    <h3>
-                      Total:
-                    </h3>
-                  </td>
-                </tr>
-                <tr>
-                  <table border="0" cellPadding="10" cellSpacing="0">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <Button>
-                            Continue Shopping
-                          </Button>
-                        </td>
-                        <td>
-                          <Button>
-                            Checkout
-                          </Button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </tr>
-              </table>
-            </tr>
+                  <tr>
+                    <table border="0" cellPadding="10" cellSpacing="0">
+                      <tbody>
+                        <tr>
+                          <td>
+                            <Button onClick={() => history.goBack()}>
+                              Continue Shopping
+                            </Button>
+                          </td>
+                          <td>
+                            <Button>
+                              Checkout
+                            </Button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </tr>
+                </table>
+              </tr>
 
 
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        ) : (
+          <p>
+            You have no items in your cart
+          </p>
+        )}
       </div>
     </div>
   );
