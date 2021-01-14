9a4a790a5abb1d806312806fbcff957e13460751
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-14 19:13:58 +0000

    Add basic forms to shipping and billing section

--- src/components/PaymentDetails/index.js
+++ src/components/PaymentDetails/index.js
@@ -1,9 +1,98 @@
 import React from 'react';
+import FormInput from './../forms/FormInput';
+import Button from './../forms/Button';
 import './styles.scss';
 
 const PaymentDetails = ({}) => {
+  const handleFormSubmit = async evt => {
+    evt.preventDefault();
+  }
+
   return (
     <div className="paymentDetails">
+      <form onSubmit={handleFormSubmit}>
+
+      <div className="group">
+        <h2>
+          Shipping Address
+        </h2>
+
+        <FormInput
+          placeholder="Recipient Name"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Line 1"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Line 2"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="City"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="State"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Postal Code"
+          type="text"
+        />
+
+      </div>
+
+      <div className="group">
+        <h2>
+          Billing Address
+        </h2>
+
+        <FormInput
+          placeholder="Cardholder's Name"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Line 1"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Line 2"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="City"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="State"
+          type="text"
+        />
+
+        <FormInput
+          placeholder="Postal Code"
+          type="text"
+        />
+
+      </div>
+
+      <div className="group">
+        <h2>
+          Card Details
+        </h2>
+      </div>
+
+      </form>
     </div>
   );
 }
