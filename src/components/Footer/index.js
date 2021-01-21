e8c99298d8f0dac4ff3b237ada8480f868fe4243
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-21 20:35:56 +0000

    Alighn header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,17 +4,19 @@ import './styles.scss';
 const Footer = props => {
   return (
     <footer className="footer">
-      <div className="wrap">
-        <div>
-          Gina Corrieri
-        </div>
-        <div className="instagram">
-          <a 
-            href="https://www.instagram.com/ginacorrieri_" 
-            target="_blank" 
-            rel="noopener noreferrer">
-              Instagram
-          </a>
+      <div className="footerMain">
+        <div className="wrap">
+          <div>
+            Gina Corrieri
+          </div>
+          <div className="instagram">
+            <a 
+              href="https://www.instagram.com/ginacorrieri_" 
+              target="_blank" 
+              rel="noopener noreferrer">
+                Instagram
+            </a>
+          </div>
         </div>
       </div>
     </footer>
