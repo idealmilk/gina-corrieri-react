4d5fd31bf3aac604f6d7c310293ffe131524d290
Matthew Gilligan <mcgilligan97@gmail.com>
2021-01-24 13:47:07 +0000

    Add measurements and colour to product form

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -17,9 +17,17 @@ const Admin = props => {
   const { products } = useSelector(mapState);
   const dispatch = useDispatch();
   const [hideModal, setHideModal] = useState(true);
-  const [productCategory, setProductCategory] = useState('mens');
+  const [productCategory, setProductCategory] = useState('tshirt');
   const [productName, setProductName] = useState('');
   const [productThumbnail, setProductThumbnail] = useState('');
+  const [productSize, setProductSize] = useState('');
+  const [productColour, setProductColour] = useState('');
+  const [productNeckline, setProductNeckline] = useState(0);
+  const [productLength, setProductLength] = useState(0);
+  const [productWidth, setProductWidth] = useState(0);
+  const [productShoulder, setProductShoulder] = useState(0);
+  const [productArmhole, setProductArmhole] = useState(0);
+  const [productWaist, setProductWaist] = useState(0);
   const [productPrice, setProductPrice] = useState(0);
   const [productDesc, setProductDesc] = useState('');
 
@@ -43,6 +51,14 @@ const Admin = props => {
     setProductCategory('mens');
     setProductName('');
     setProductThumbnail('');
+    setProductSize('');
+    setProductColour('');
+    setProductNeckline(0);
+    setProductLength(0);
+    setProductWidth(0);
+    setProductShoulder(0);
+    setProductArmhole(0);
+    setProductWaist(0);
     setProductPrice(0);
     setProductDesc('');
   };
@@ -55,6 +71,14 @@ const Admin = props => {
         productCategory,
         productName,
         productThumbnail,
+        productSize,
+        productColour,
+        productNeckline,
+        productLength,
+        productWidth,
+        productShoulder,
+        productArmhole,
+        productWaist,
         productPrice,
         productDesc,
       })
@@ -100,11 +124,14 @@ const Admin = props => {
             <FormSelect
               label="Category"
               options={[{
-                value: "mens",
-                name: "Mens"
+                value: "tshirt",
+                name: "T Shirt"
               }, {
-                value: "womens",
-                name: "Womens"
+                value: "bag",
+                name: "Bag"
+              }, {
+                value: "trousers",
+                name: "Trousers"
               }]}
               handleChange={e => setProductCategory(e.target.value)}
             />
@@ -121,6 +148,80 @@ const Admin = props => {
               type="url"
               value={productThumbnail}
               handleChange={e => setProductThumbnail(e.target.value)}
+            />
+
+            <FormInput
+              label="Size"
+              type="text"
+              value={productSize}
+              handleChange={e => setProductSize(e.target.value)}
+            />
+
+            <FormInput
+              label="Colour"
+              type="text"
+              value={productColour}
+              handleChange={e => setProductColour(e.target.value)}
+            />
+
+            <FormInput
+              label="Neckline"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productNeckline}
+              handleChange={e => setProductNeckline(e.target.value)}
+            />
+
+            <FormInput
+              label="Length"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productLength}
+              handleChange={e => setProductLength(e.target.value)}
+            />
+
+            <FormInput
+              label="Width"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productWidth}
+              handleChange={e => setProductWidth(e.target.value)}
+            />
+
+            <FormInput
+              label="Shoulder"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productShoulder}
+              handleChange={e => setProductShoulder(e.target.value)}
+            />
+
+            <FormInput
+              label="Armhole"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productArmhole}
+              handleChange={e => setProductArmhole(e.target.value)}
+            />
+
+            <FormInput
+              label="Waist"
+              type="number"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              value={productWaist}
+              handleChange={e => setProductWaist(e.target.value)}
             />
 
             <FormInput
