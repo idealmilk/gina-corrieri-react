c37fa41818e97c8bda20163351c6c97d8acd741e
Matthew Gilligan <mcgilligan97@gmail.com>
2021-02-20 13:43:56 +0000

    Add no product message

--- src/components/ProductResults/index.js
+++ src/components/ProductResults/index.js
@@ -6,78 +6,75 @@ import Product from './Product';
 import './styles.scss';
 
 const mapState = ({ productsData }) => ({
-  products: productsData.products
+    products: productsData.products
 });
 
 const ProductResults = ({ }) => {
-  const dispatch = useDispatch();
-  const { filterType } = useParams();
-  const { products } = useSelector(mapState);
+    const dispatch = useDispatch();
+    const { filterType } = useParams();
+    const { products } = useSelector(mapState);
 
-  console.log(filterType)
+    console.log(filterType)
 
-  const { data } = products;
+    const { data } = products;
 
-  console.log(data);
+    console.log(data);
 
-  useEffect(() => {
-    dispatch(
-      fetchProductsStart({ filterType })
-    )
-  }, [filterType]);
+    useEffect(() => {
+        dispatch(
+            fetchProductsStart({ filterType })
+        )
+    }, [filterType]);
 
-  if (!Array.isArray(data)) return null;
-  if (data.length < 1) {
+    const filter = 
+        <div className="filter">
+            <div className="wrap">
+                <p>
+                    {filterType === undefined ? "Shop All" : <a href="/shop">Shop All</a>}
+                </p>
+                <p>
+                    {filterType === "tshirt" ? "T Shirts" : <a href="/shop/tshirt">T Shirts</a>}
+                </p>
+                <p>
+                    {filterType === "trousers" ? "Trousers" : <a href="/shop/trousers">Trousers</a>}
+                </p>
+                <p>
+                    {filterType === "bag" ? "Bags" : <a href="/shop/bag">Bags</a>}
+                </p>
+            </div>
+        </div>
+
+    if (!Array.isArray(data)) return null;
+    
+    if (data.length < 1) {
+        return (
+            <div className="productResults">
+                {filter}
+                
+            </div>
+        );
+    }
+
     return (
-      <div className="products">
-        <p>
-          No search results.
-        </p>
-      </div>
-    );
-  }
-
-  return (
-    <div className="productResults">
-      <div className="filter">
-        <div className="wrap">
-          <p>
-            {filterType === undefined ? "Shop All" : <a href="/shop">Shop All</a>}
-          </p>
-          <p>
-            {filterType === "tshirt" ? "T Shirts" : <a href="/shop/tshirt">T Shirts</a>}
-          </p>
-          <p>
-            {filterType === "trousers" ? "Trousers" : <a href="/shop/trousers">Trousers</a>}
-          </p>
-          <p>
-            {filterType === "bag" ? "Bags" : <a href="/shop/bag">Bags</a>}
-          </p>
-        </div>
-      </div>
-
-      <div className="products">
-
         <div className="productResults">
-          {data.map((product, pos) => {
-            const { productThumbnail, productName, productPrice, productQuantity } = product;
-            if (!productThumbnail || !productName || !productPrice ||
-              typeof productPrice === 'undefined') return null;
+            {filter}
+            <div className="products">
+                {data.map((product, pos) => {
+                    const { productThumbnail, productName, productPrice, productQuantity } = product;
+                    if (!productThumbnail || !productName || !productPrice ||
+                        typeof productPrice === 'undefined') return null;
 
-            const configProduct = {
-              ...product
-            };
+                    const configProduct = {
+                    ...product
+                    };
 
-            return (
-              <Product key={pos} {...configProduct} />
-            );
-          })}
+                    return (
+                    <Product key={pos} {...configProduct} />
+                    );
+                })}
+            </div>
         </div>
-
-      </div>
-
-    </div>
-  );
+    );
 };
 
 export default ProductResults;
