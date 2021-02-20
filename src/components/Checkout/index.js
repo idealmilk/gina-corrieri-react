ab21d258f080f9be7956c577aa1bab39a2b05969
Matthew Gilligan <mcgilligan97@gmail.com>
2021-02-20 17:06:50 +0000

    Add style to rest of checkout

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -20,106 +20,101 @@ const Checkout = ({ }) => {
 
   return (
     <div className="checkout">
-      <h1>
-        Checkout
-      </h1>
-
       <div className="cart">
         {cartItems.length > 0 ? (
-          <table border="0" cellPadding="0" cellSpacing="0">
-            <tbody>
-              <tr>
-                <td>
-                  <table className="checkoutHeader" border="0" cellPadding="10" cellSpacing="0">
-                    <tbody>
-                      <tr>
-                        <th>
-                          Product
-                        </th>
-                        <th>
-                          Description
-                        </th>
-                        <th>
-                          Quantity
-                        </th>
-                        <th>
-                          Price
-                        </th>
-                        <th>
-                          Remove
-                        </th>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <table border="0" cellSpacing="0" cellPadding="0">
-                    <tbody>
-                      {cartItems.map((item, pos) => {
-                        return (
-                          <tr key={pos}>
-                            <td>
-                              <Item {...item} />
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <table border="0" cellSpacing="0" cellPadding="0">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <table border="0" cellPadding="10" cellSpacing="0">
-                            <tbody>
-                              <tr>
-                                <td>
-                                <h3>
-                                  Total: £{total}
-                                </h3>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <table border="0" cellPadding="10" cellSpacing="0">
-                            <tbody>
-                              <tr>
-                                <td>
-                                  <Button onClick={() => history.goBack()}>
-                                    Continue Shopping
-                                  </Button>
-                                </td>
-                                <td>
-                                  <Button onClick={() => history.push('/payment')}>
-                                    Checkout
-                                  </Button>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div>
+            <table border="0" cellPadding="0" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td>
+                    <table className="checkoutHeader" border="0" cellPadding="10" cellSpacing="0">
+                      <tbody>
+                        <tr>
+                          <th>
+                            Product
+                          </th>
+                          <th>
+                            Name
+                          </th>
+                          <th>
+                            Quantity
+                          </th>
+                          <th>
+                            Price
+                          </th>
+                          <th>
+                            Remove
+                          </th>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td  className="items">
+                    <table border="0" cellSpacing="0" cellPadding="0">
+                      <tbody>
+                        {cartItems.map((item, pos) => {
+                          return (
+                            <tr key={pos}>
+                              <td>
+                                <Item {...item} />
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+            <div className="breakdown">
+              <div className="calculation">
+                <div className="subtotal">
+                  <div className="key">
+                    <p>Subtotal</p>
+                  </div>
+                  <div className="value">
+                    <p>£{total}</p>
+                  </div>
+                </div>
+                <div className="shipping">
+                  <div className="key">
+                    <p>Shipping</p>
+                  </div>
+                  <div className="value">
+                    <p>£2.75</p>
+                  </div>
+                </div>
+                <div className="total">
+                  <div className="key">
+                    <p>Total</p>
+                  </div>
+                  <div className="value">
+                    <p>£{total + 2.75}</p>
+                  </div>
+                </div>
+              </div>
+
+              <Button onClick={() => history.push('/payment')}>
+                Proceed to Checkout
+              </Button>
+            </div>
+
+            {/* <Button onClick={() => history.goBack()}>
+              Continue Shopping
+            </Button> */}
+          </div>
+                               
+            
         ) : (
             <p>
               {errMsg}
             </p>
           )}
+        
       </div>
     </div>
   );
