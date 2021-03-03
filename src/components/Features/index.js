fbaf0eb9362b3aa62d9bf27862fc2bc54d448b37
Matthew Gilligan <mcgilligan97@gmail.com>
2021-03-03 19:33:21 +0000

    Add array object for features

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -2,6 +2,34 @@ import React from 'react';
 import './styles.scss';
 import Feature from './Feature';
 
+const features = [
+  {
+    url: "https://www.youtube.com/embed/qJhyhaf_uRs",
+    desc: "Amaarae wearing Flame trousers in red/yellow in her Colors video of ‘Leave Me Alone’",
+    styledBy: "Styled by Natalie Roar and assisted by Emmanouela Megkistou",
+  },
+  {
+    url: "https://www.youtube.com/embed/dqJ8oud7l2Q",
+    desc: "Mars leather jacket and Flame trousers in brown/orange worn by cast member in the Biig Piig music video for ‘Feels Right’",
+    styledBy: "Styled by Suzie Walsh",
+  },
+  {
+    url: "https://www.youtube.com/embed/csVTig5bqFE",
+    desc: "Master Peace wearing Venus and Neptune necklaces in his music video for ‘Love Bites’",
+    styledBy: "Styled by Kayla Garner-Jones",
+  },
+  {
+    url: "https://www.youtube.com/embed/uc72XIXoZ2g",
+    desc: "Master Peace wearing Eternal Flame jacket and bleached corduroy panel trousers in his music video for ‘Party Never Ends’",
+    styledBy: "Styled by Kayla Garner-Jones",
+  },
+  {
+    url: "https://www.youtube.com/embed/H0kAAoahJQM",
+    desc: "Haiyti wearing Eternal Flame jacket and bleached corduroy panel trousers in her music video for ‘Was hast du damit zu tun’",
+    styledBy: "Styled by Cynthia Igbokwe",
+  }
+];
+
 const Features = props => {
   return (
     <div className="features">
--- src/layouts/HomepageLayout.js
+++ src/layouts/HomepageLayout.js
@@ -3,13 +3,13 @@ import Header from './../components/Header';
 import Footer from './../components/Footer';
 
 const HomepageLayout = props => {
-  return (
-    <div className="fullHeight">
-      <Header {...props} />
-      {props.children}
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="fullHeight">
+			<Header {...props} />
+			{props.children}
+			<Footer />
+		</div>
+	);
 };
 
 export default HomepageLayout;
