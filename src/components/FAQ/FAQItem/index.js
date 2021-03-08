251ffb78cd2359ce03e8968c3f283531a44a5aea
Matthew Gilligan <mcgilligan97@gmail.com>
2021-03-08 13:14:03 +0000

    Add marked to long text fields

--- src/components/FAQ/FAQItem/index.js
+++ src/components/FAQ/FAQItem/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import marked from 'marked';
 
 const FAQItem = ({ item }) => {
     const { question, answer } = item.fields;
-    
+
+    const itemAnswer = marked(answer);
+
 	return (
 		<div className="item">
             <h3>{question}</h3>
-            <p>{answer}</p>
+            <p dangerouslySetInnerHTML={{__html: itemAnswer}} />
 		</div>
 	);
 }
--- src/components/Features/Feature/index.js
+++ src/components/Features/Feature/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import marked from 'marked';
 
 const Feature = ({ feature }) => {
 	console.log(feature);
 	const { url, description, styledBy } = feature.fields;
 
+	const featureDesc = marked(description);
+
 	return (
 		<div className="feature">
-			<p className="desc">{description}</p>
+			<p dangerouslySetInnerHTML={{__html: featureDesc}} />
 			<p className="styledBy">{styledBy}</p>
 			<iframe width="600" height="350" src={url} frameborder="0" title="YouTube" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
