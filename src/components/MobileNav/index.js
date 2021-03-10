0dcd00cde93695617f3e3b34b6f638150d2a91c9
Matthew Gilligan <mcgilligan97@gmail.com>
2021-03-10 18:56:12 +0000

    Add cart to mobile nav

--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import styled from "styled-components";
-import HamburgerMenu from "./HamburgerMenu";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { FaShoppingCart } from "react-icons/fa";
+import HamburgerMenu from './HamburgerMenu';
+import Logo from './../../assets/transparency_logo.gif';
 
 const NavbarContainer = styled.div`
   width: 100%;
@@ -15,10 +18,51 @@ const NavbarContainer = styled.div`
   z-index: 1000;
 `;
 
+const NavbarLogo = styled.div`
+	width: 13rem;
+	position: absolute;
+	top: 50%;
+	left: 0;
+	margin-left: 4px;
+	-webkit-transform: translateY(-50%);
+	-moz-transform: translateY(-50%);
+	transform: translateY(-50%);
+	img {
+		display: block;
+		width: 100%;
+		margin: 0;
+	}
+`;
+
+const NavbarIcons = styled.div`
+	display: flex;
+	position: absolute;
+	top: 50%;
+	right: 0;
+	font-size: 2.8rem;
+	margin-right: 65px;
+	-webkit-transform: translateY(-50%);
+	-moz-transform: translateY(-50%);
+	transform: translateY(-50%);
+	a {
+		color: black;
+	}
+`;
+
 const MobileNav = (props) => {
   return (
     <NavbarContainer>
-      <HamburgerMenu />
+		<NavbarLogo>
+			<Link to="/">
+				<img src={Logo} alt="SimpleTut LOGO" />
+			</Link>
+		</NavbarLogo>
+		<NavbarIcons>
+			<Link to="/cart">
+				<FaShoppingCart />
+			</Link>
+		</NavbarIcons>
+		<HamburgerMenu />	
     </NavbarContainer>
   );
 };
